23483c0ff6f529b572b50640cecc737efc7d22ae
allahyarceferoff <callahyar@inbox.ru>
2023-02-24 09:28:20 +0400

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Brush randomColor;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is Button btn)
+            {
+                Grid1.Children.Remove(btn);
+                this.Title = btn.Content.ToString();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn)
+            {
+                Random r = new Random();
+                randomColor = new SolidColorBrush(Color.FromRgb((byte)r.Next(1, 255), (byte)r.Next(1, 255), (byte)r.Next(1, 233)));
+                btn.Background = randomColor;
+                MessageBox.Show($"Clicked {btn.Content} button", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+    }
+}
